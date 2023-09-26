38b1770a1ca026a0e85d115864364f2659d8aea8
Sylwester Mielniczuk <flaboy.com@gmail.com>
2023-09-26 15:18:35 +0100

    Gallery - thumbnails vs outputs generator fix 3

--- scripts/generate_depth_thumbnails.sh
+++ scripts/generate_depth_thumbnails.sh
@@ -33,8 +33,10 @@ create_thumbnail() {
     local thumbnail_path="${image_path%.*}_th.${image_path##*.}"
 
     # Check if the corresponding depth image exists
-    local depth_image_path="$OUTPUT_DIR/${image_path##*/}"
-    depth_image_path="${depth_image_path%.*}_depth.${depth_image_path##*.}"
+    local depth_image_basename="${image_path##*/}"
+    local depth_image_name="${depth_image_basename%.*}"
+    local depth_image_extension="${depth_image_basename##*.}"
+    local depth_image_path="$OUTPUT_DIR/${depth_image_name}_depth.${depth_image_extension,,}"  # Convert extension to lowercase
 
     if [[ -f "$depth_image_path" ]]; then
         if [[ ! -f "$thumbnail_path" ]]; then
@@ -47,6 +49,7 @@ create_thumbnail() {
         echo "No depth image for $image_path. Skipping thumbnail creation."
     fi
 }
+
 
 # Loop through each file in the image directory
 for filepath in "$IMAGE_DIR"/*; do
